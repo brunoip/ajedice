8eefbb30544705c05458809f50bae0edc27e8a1b
Bruno Pasquini <brunopasquini@hotmail.com>
2025-06-05 21:03:22 -0300

    play button

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
 	selector: 'app-menu',
 	standalone: true,
-	imports: [RouterModule],
+	imports: [RouterModule, NgStyle],
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit  {
+
+	backgroundImage = 'assets/title_image_text.png';
+
+		ngOnInit() {
+		this.updateBackgroundImage();
+	}
+
+	@HostListener('window:resize')
+	onResize() {
+		this.updateBackgroundImage();
+	}
+
+	updateBackgroundImage() {
+		const isPortraitMobile = window.innerWidth <= 768 && window.innerHeight > window.innerWidth;
+		this.backgroundImage = isPortraitMobile
+			? 'assets/title_image.png'
+			: 'assets/title_image_text.png';
+	}
+}
